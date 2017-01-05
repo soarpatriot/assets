822a7be9030801b41d18c8df4072937a924175c6
Soar <85624529@qq.com>
2017-01-05 21:45:08 +0800

    change shared config

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :repo_url, 'git@github.com:soarpatriot/assets.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/process.yml')
+set :linked_files, fetch(:linked_files, []).push('config/process.yml', 'config/app.json')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 
