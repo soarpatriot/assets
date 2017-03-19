03c3e678dfcf3a39112b4ac456f9a9d0891b5549
Soar <85624529@qq.com>
2017-03-20 00:08:20 +0800

    add delete apis

--- router/file.js
+++ router/file.js
@@ -1,6 +1,7 @@
 import Router from 'koa-router'
 import { upload } from './../helper'
-
+import path from 'path'
+import fs from 'fs'
 const file = new Router({
 })
 
@@ -19,5 +20,23 @@ file
   .post('/files', upload.any(), async ctx => {
     ctx.body = ctx.req.files
   })
+  .delete('/files/:name', ctx => {
+    let name = ctx.params.name
+    let query = ctx.request.query
+    // let relative = ctx.params.relative
+    // console.log(`relative: ${query.relative}`)
+    const fileRelative = '../static/upload' + query.relative
+    let filePath = path.resolve(__dirname, fileRelative)
+
+    // console.log(`filePath: ${filePath}`)
+    fs.unlink(filePath)
+    // console.log(`file: ${filePath}`)
+    const result = {
+      code: 0,
+      message: 'success'
+    }
+    ctx.body = result
+  })
+
 
 export default file
