0c0999c18dd04d038f7d1ffacd75c8496b57e708
Soar <85624529@qq.com>
2018-02-24 10:41:53 +0800

    fixed the material not exist bug

--- router/file.js
+++ router/file.js
@@ -115,15 +115,14 @@ file
       }
     }
     const assets = await f.listDir(unzipPath)
-    logger.info('await...')
     const fa = f.flat(assets)
     const ff = f.regenRelative(fa, unzipPath)
-    logger.info('regen...')
     const item = f.mArray(ff)
-    const matDir = f.matPath(fullPath, item.name)
-    logger.info('matn...')
     file.list = ff
-    file.murl = matDir
+    if (item) {
+      const matDir = f.matPath(fullPath, item.name)
+      file.murl = matDir
+    }
     file.parent = fullPath
     ctx.body = ctx.req.file
   })
